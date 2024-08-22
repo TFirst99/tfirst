703f9a101aadb37e7332c89e8659346756a3884f
tfirst99 <19269633+TFirst99@users.noreply.github.com>
2024-08-21 20:46:27 -0400

    added cache to spotify api

--- functions/api/spotify.js
+++ functions/api/spotify.js
@@ -1,22 +1,40 @@
 export async function onRequest(context) {
   const { SPOTIFY_KV } = context.env;
+  const cache = caches.default;
 
-  try {
-    const spotifyData = await SPOTIFY_KV.get('current_track', 'json');
-    if (spotifyData) {
-      return new Response(JSON.stringify(spotifyData), {
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } else {
-      return new Response(JSON.stringify({ error: 'No data available' }), {
-        status: 404,
-        headers: { 'Content-Type': 'application/json' },
-      });
+  let response = await cache.match(context.request);
+
+  if (!response) {
+    try {
+      const spotifyData = await SPOTIFY_KV.get("current_track", "json");
+      if (spotifyData) {
+        response = new Response(JSON.stringify(spotifyData), {
+          headers: {
+            "Content-Type": "application/json",
+            "Cache-Control": "public, max-age=55",
+          },
+        });
+      } else {
+        response = new Response(
+          JSON.stringify({ error: "No data available" }),
+          {
+            status: 404,
+            headers: { "Content-Type": "application/json" },
+          },
+        );
+      }
+
+      context.waitUntil(cache.put(context.request, response.clone()));
+    } catch (error) {
+      response = new Response(
+        JSON.stringify({ error: "Error fetching Spotify data" }),
+        {
+          status: 500,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
     }
-  } catch (error) {
-    return new Response(JSON.stringify({ error: 'Error fetching Spotify data' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
   }
+
+  return response;
 }
