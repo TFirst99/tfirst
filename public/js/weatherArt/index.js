dc9fb617d2b61d8c2c1c8eaf1409afc61850bf46
tfirst <19269633+TFirst99@users.noreply.github.com>
2024-08-22 18:04:15 -0400

    refactored weather art into separate files
    - move weather art into separate files in the weatherArt directory
    - create an index.js file that consolidates and exports all weather art
    - update WeatherManager class to import weather art from external files

---
+++ public/js/weatherArt/index.js
@@ -0,0 +1,17 @@
+import clear from './clear.js';
+import clouds from './clouds.js';
+import rain from './rain.js';
+import snow from './snow.js';
+import mist from './mist.js';
+import drizzle from './drizzle.js';
+import thunderstorm from './thunderstorm.js';
+
+export default {
+  clear,
+  clouds,
+  rain,
+  snow,
+  mist,
+  drizzle,
+  thunderstorm
+};
