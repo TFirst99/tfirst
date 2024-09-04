37ee31b1776079cddd607d69a9d7165cb39591c1
tfirst <19269633+TFirst99@users.noreply.github.com>
2024-09-04 18:22:10 -0400

    separate about widget
    separate the about widget into 3 boxes

--- public/js/AboutWidgetManager.js
+++ public/js/AboutWidgetManager.js
@@ -2,9 +2,13 @@ import { WidgetUtil } from '/js/utils/widgetUtil.js';
 
 export class AboutWidgetManager {
   constructor() {
-    this.widgetElement = document.getElementById("about-widget");
+    this.titleElement = document.getElementById("about-title-widget");
+    this.studentElement = document.getElementById("about-student-widget");
+    this.personalElement = document.getElementById("about-personal-widget");
+    this.titleWidget = new WidgetUtil(this.titleElement, { width: 21 });
+    this.studentWidget = new WidgetUtil(this.studentElement, { width: 21 });
+    this.personalWidget = new WidgetUtil(this.personalElement, { width: 21 });
     this.jsonUrl = "/about.json";
-    this.widgetUtil = new WidgetUtil(this.widgetElement);
   }
 
   async updateAboutData() {
@@ -23,16 +27,26 @@ export class AboutWidgetManager {
 
   updateWidget(data) {
     if (data) {
-      this.widgetUtil.updateWidget(
-        "ABOUT ME",
-        ...data.student.map(line => ({ content: line })),
-        ...data.personal.map(line => ({ content: line }))
+      this.titleWidget.updateWidget(
+        "ABOUT ME"
+      );
+      this.studentWidget.updateWidget(
+        "STUDENT",
+        ...data.student.slice(2).map(line => ({ content: line }))
+      );
+      this.personalWidget.updateWidget(
+        "PERSONAL",
+        ...data.personal.slice(2).map(line => ({ content: line }))
       );
     } else {
-      this.widgetUtil.updateWidget(
-        "ABOUT ME",
-        " ",
-        "No data available."
+      this.titleWidget.updateWidget(
+        "ABOUT ME"
+      );
+      this.studentWidget.updateWidget(
+        "if you're seeing this"
+      );
+      this.personalWidget.updateWidget(
+        "it didn't load lol."
       );
     }
   }
