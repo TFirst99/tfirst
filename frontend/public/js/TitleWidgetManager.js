a75e3d80a924cc0798f0f360c3ceda412fecd5fd
tfirst <19269633+TFirst99@users.noreply.github.com>
2024-09-09 13:06:52 -0400

    adding a smaller title

--- frontend/public/js/TitleWidgetManager.js
+++ frontend/public/js/TitleWidgetManager.js
@@ -7,10 +7,29 @@ export class TitleWidgetManager {
 |_   _|_ _|  \\/  |  ___|_ _|  _ \\/ ___|_   _|___ ___  _ __ ___  
   | |  | || |\\/| | |_   | || |_) \\___ \\ | | / __/ _ \\| '_ \` _ \\ 
   | |  | || |  | |  _|  | ||  _ < ___) || || (_| (_) | | | | | |
-  |_| |___|_|  |_|_|   |___|_| \\_\\____/ |_(_)___\\___/|_| |_| |_|`;
+  |_| |___|_|  |_|_|   |___|_| \\_\\____/ |_(_)___\\___/|_| |_| |_|`;,
+  
+  this.smallTitleText =
+` _____ ___ __  __ _____ ___ ____  ____ _____ 
+|_   _|_ _|  \\/  |  ___|_ _|  _ \\/ ___|_   _|
+  | |  | || |\\/| | |_   | || |_) \\___ \\ | |  
+  | |  | || |  | |  _|  | ||  _ < ___) || |  
+  |_| |___|_|  |_|_|   |___|_| \\_\\____/ |_|  `;
+  
   }
-
   init() {
-    this.container.innerHTML = this.titleText;
+    //main title
+    const mainTitleContainer = document.createElement('div');
+    mainTitleContainer.className = 'main-title';
+    mainTitleContainer.innerHTML = this.titleText;
+
+    //small title
+    const smallTitleContainer = document.createElement('div');
+    smallTitleContainer.className = 'small-title';
+    smallTitleContainer.textContent = this.smallTitleText;
+
+    //append to container
+    this.container.appendChild(mainTitleContainer);
+    this.container.appendChild(smallTitleContainer);
   }
 }
